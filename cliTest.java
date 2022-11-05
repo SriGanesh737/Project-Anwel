2c84d31a2a25d494931617c400f5c1f587e3a423
andavarapulikith <likithandavarapuiiits@gmail.com>
2022-11-05 16:54:03 +0530

    new msg

--- cliTest.java
+++ cliTest.java
@@ -13,15 +13,15 @@ public class cliTest
             ZooManagement.StoreAnimal();
              ZooManagement.PrintAllAnimalDetails();
         }
-        else if (key == 2)
-        {
-            System.out.println("This is function-2");
-            ZooManagement.storeemployees();
-            //employee e2 = new manager("suresh", 1030, 70000);
-            manager m1 = new manager();
-            m1.sendmessage(105, "m-1","demo message sent");
-            ZooManagement.demofunc(105);
-        }
+//        else if (key == 2)
+//        {
+//            System.out.println("This is function-2");
+//            ZooManagement.storeemployees();
+//            //employee e2 = new manager("suresh", 1030, 70000);
+//            manager m1 = new manager();
+//            m1.sendmessage(105, "m-1","demo message sent");
+//            ZooManagement.demofunc(105);
+//        }
         else if (key == 3)
         {
             System.out.println("This is function-3");
