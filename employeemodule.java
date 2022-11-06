1b40229f8fd4c414c2ce1e4b372f05a16bce62d6
Sri Ganesh <sriganeshiiits@gmail.com>
2022-11-06 15:54:38 +0530

    Added  add an employee to their corresponding tables function

--- employeemodule.java
+++ employeemodule.java
@@ -10,7 +10,7 @@ interface duties_and_salaries {
 abstract class employee implements duties_and_salaries {
 
     String name;
-    int salary;
+    int salary=0;
     int id;
 
     public employee() {
@@ -47,6 +47,115 @@ abstract class employee implements duties_and_salaries {
         return id;
     }
 
+    public void addEmployee() {
+
+        // we should execute differnt queries based on the type
+        int type = -1;
+        /*
+         * type=1-->manager
+         * type=2-->Caretaker
+         * type=3-->Doctor
+         * type=4-->ChartedAccountant
+         * type=5-->ZooGuide
+         */
+
+        if (this instanceof manager)
+            type = 1;
+        else if (this instanceof caretaker)
+            type = 2;
+        else if (this instanceof doctor)
+            type = 3;
+        else if (this instanceof ChartedAccountant)
+            type = 4;
+        else if (this instanceof ZooGuide)
+            type = 5;
+
+
+        Database db = new Database();
+
+        try {
+            db.Establish();
+        } catch (Exception e1) {
+            // TODO Auto-generated catch block
+            e1.printStackTrace();
+        }
+
+        switch (type) {
+            case 1:
+                manager y1 = (manager) this;
+                db.setQuery(String.format("insert into manager values(%d,\"%s\",%d)",y1.getId(),y1.getName(),y1.getSalary()));
+                break;
+            case 2:
+                caretaker y2 = (caretaker) this;
+                db.setQuery(String.format("insert into caretaker values(%d,\"%s\",%d)", y2.getId(), y2.getName(),
+                        y2.getSalary()));
+                break;
+            case 4:
+                ChartedAccountant y4 = (ChartedAccountant) this;
+                db.setQuery(String.format("insert into chartedaccountant values(%d,\"%s\",%d,%d)", y4.getId(), y4.getName(),
+                        y4.getSalary(),y4.currentFinancialPosition));
+                break;
+            case 3:
+                doctor y3 = (doctor) this;
+                db.setQuery(String.format("insert into doctor values(%d,\"%s\",%d,%d)", y3.getId(), y3.getName(),
+                        y3.getSalary(),y3.CasesTaken));
+                break;
+            case 5:
+                ZooGuide y5 = (ZooGuide) this;
+                db.setQuery(String.format("insert into zooguide values(%d,\"%s\",%d,%f)", y5.getId(), y5.getName(),
+                        y5.getSalary(),y5.getAvgrating()));
+                break;
+            default:
+                System.out.println("In the Default Statement");
+        }
+
+
+
+        try {
+            int rs = db.Update();
+        } catch (SQLException e) {
+            System.out.println("here bro while adding message to database error occurred!!+_+ O_O");
+            e.printStackTrace();
+        }
+
+        try {
+            db.Close(db.getSt(), db.getCon());
+        } catch (SQLException e) {
+            System.out.println("Error in closing ¬_¬");
+            e.printStackTrace();
+        }
+    }
+
+    // public void update_salary_IN_Database() {
+    //     Database db = new Database();
+
+    //     try {
+    //         db.Establish();
+    //     } catch (Exception e1) {
+    //         // TODO Auto-generated catch block
+    //         e1.printStackTrace();
+    //     }
+
+    //     // need to test this line of code .
+    //     db.setQuery(String.format("update employee set salary=%d where id= %d", salary, id));
+
+    //     try {
+    //         int rs = db.Update();
+    //         System.out.println(rs);
+    //     } catch (SQLException e) {
+
+    //         System.out.println("while updating salary...");
+    //         e.printStackTrace();
+    //     }
+
+    //     try {
+    //         db.Close(db.getSt(), db.getCon());
+    //     } catch (SQLException e) {
+    //         System.out.println("Error in closing ¬_¬");
+    //         e.printStackTrace();
+    //     }
+    // }
+
 }
 
 class manager extends employee {
@@ -60,38 +169,35 @@ class manager extends employee {
         super.salary = salary;
     }
 
-    public void duty()
-    {
-          System.out.println("Zoo manager and head of Zoo");
+    public void duty() {
+        System.out.println("Zoo manager and head of Zoo");
     }
 
-    public void sendmessage(int recieversid, String msgid, String body)
-    {
+    public void sendmessage(int recieversid, String msgid, String body) {
         // id is for whom we want to send
         message m = new message(msgid, getId(), body);
         m.setReceiversId(recieversid);
         // for (employee x : ZooManagement.el) {
-        //     if (x.getId() == recieversid) {
-        //         if(x instanceof caretaker)
-        //         {
-        //             caretaker y = (caretaker) x;
-        //             y.inbox.add(m);
-        //         }
-        //         else if (x instanceof doctor)
-        //         {
-
-        //         }
-
-        //         m.storeMessage(); // store message in the database
-        //         ZooManagement.ml.add(m); // store all the messages
-        //         System.out.println("Message Sent");
-        //     }
+        // if (x.getId() == recieversid) {
+        // if(x instanceof caretaker)
+        // {
+        // caretaker y = (caretaker) x;
+        // y.inbox.add(m);
         // }
-      m.storeMessage();
+        // else if (x instanceof doctor)
+        // {
+
+        // }
+
+        // m.storeMessage(); // store message in the database
+        // ZooManagement.ml.add(m); // store all the messages
+        // System.out.println("Message Sent");
+        // }
+        // }
+        m.storeMessage();
     }
 
-    public void salary()
-    {
+    public void salary() {
         this.salary = 100000;
 
     }
@@ -107,16 +213,16 @@ class caretaker extends employee {
         super.setId(id);
     }
 
-    //not required since we have a better function in message class
+    // not required since we have a better function in message class
     // public void showInbox() {
-    //     System.out.println("The messages in the inbox are :");
-    //     for (message x : inbox) {
-    //         if (x.getMarkAsRead() == 0) {
-    //             System.out.println(x.getBody());
-    //             x.setMarkAsRead(1);
-    //             x.markRead(); //update in database
-    //         }
-    //     }
+    // System.out.println("The messages in the inbox are :");
+    // for (message x : inbox) {
+    // if (x.getMarkAsRead() == 0) {
+    // System.out.println(x.getBody());
+    // x.setMarkAsRead(1);
+    // x.markRead(); //update in database
+    // }
+    // }
     // }
 
     public void sendMessage(int receiversId, String msgId, String body) {
@@ -126,13 +232,13 @@ class caretaker extends employee {
         m.setReceiversId(receiversId);
         // System.out.println("here2.3");
         // for (employee x : ZooManagement.el) {
-        //     if (x.getId() == receiversId) {
-        //         doctor y = (doctor) x;
-        //         y.inbox.add(m);
-        //         m.storeMessage(); //store message in the database
-        //         ZooManagement.ml.add(m); //store all the messages
-        //         System.out.println("Message Sent to doctor");
-        //     }
+        // if (x.getId() == receiversId) {
+        // doctor y = (doctor) x;
+        // y.inbox.add(m);
+        // m.storeMessage(); //store message in the database
+        // ZooManagement.ml.add(m); //store all the messages
+        // System.out.println("Message Sent to doctor");
+        // }
         // }
         m.storeMessage();
     }
@@ -152,7 +258,7 @@ class doctor extends employee {
 
     ArrayList<message> inbox = new ArrayList<message>();
     ArrayList<String> patientList = new ArrayList<>();
-    private int CasesTaken = 0;
+     int CasesTaken = 0;
 
     public doctor() {
 
@@ -211,7 +317,7 @@ class ZooGuide extends employee {
     // give bonus based on the ratings they got.
     ArrayList<Integer> ratings = new ArrayList<>();
     ArrayList<message> inbox = new ArrayList<message>();
-    private double avgrating;
+    private double avgrating=0;
 
     public ZooGuide(int baseSalary) {
         this.baseSalary = baseSalary;
@@ -265,9 +371,7 @@ class ZooGuide extends employee {
 
 }
 
-
-
-class sponcers extends Database{
+class sponcers extends Database {
 
     String Name;
     int donatedAmount;
@@ -289,8 +393,7 @@ class sponcers extends Database{
         Name = name;
     }
 
-    public int getDonatedAmount()
-    {
+    public int getDonatedAmount() {
         return donatedAmount;
     }
 
@@ -298,8 +401,7 @@ class sponcers extends Database{
         this.donatedAmount = donatedAmount;
     }
 
-    public void storeSponcer()
-    {
+    public void storeSponcer() {
         Database db = new Database();
 
         try {
@@ -332,23 +434,34 @@ class sponcers extends Database{
 
 }
 
-public class employeemodule {
+public  class employeemodule {
 
     public static void main(String args[]) {
-        doctor doc1 = new doctor("ram", 108, 200000);
-        caretaker ct1 = new caretaker("raj", 106);
-        manager m1 = new manager("hari", 101, 300000);
 
-        // ZooManagement.el.add(m1);
-        // ZooManagement.el.add(doc1);
-        // ZooManagement.el.add(ct1);
-        new message().showUnreadMessages(108);
+        employee doc1 = new doctor("ram", 108, 200000);
+        employee ct1 = new caretaker("raj", 106);
+        employee m1 = new manager("hari", 101, 300000);
+        employee ca1 = new ChartedAccountant("Ramesh", 333);
+        employee zg1 = new ZooGuide("harry", 205, 30000);
 
-        m1.sendmessage(106, "c-150", "Assemble here at hall");
-        ct1.sendMessage(108, "S-200", "A unread message for the second time ");
-        //ZooManagement.displayAllMessages();
-        System.out.println("here");
-        m1.sendmessage(106, "c-157", "Assemble here at room");
-        new message().showUnreadMessages(108);
+        // new message().showUnreadMessages(108);
+
+        // m1.sendmessage(106, "c-150", "Assemble here at hall");
+        // ct1.sendMessage(108, "S-200", "A unread message for the second time ");
+        // // ZooManagement.displayAllMessages();
+        // System.out.println("here");
+        // m1.sendmessage(106, "c-157", "Assemble here at room");
+        // new message().showUnreadMessages(108);
+
+        doc1.addEmployee();
+
+        ct1.addEmployee();
+
+        m1.addEmployee();
+
+        ca1.addEmployee();
+
+        zg1.addEmployee();
+
     }
 }
